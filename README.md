fca1d26b811ee1a1abd985b150eb64a6d8af0b91
webmaster@simonhuwiler.ch <webmaster@simonhuwiler.ch>
2022-12-22 17:12:11 +0100

    wip

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ gcloud init
 ```
 gcloud functions deploy apiproxy --runtime python310 --trigger-http --allow-unauthenticated
 ```
+
+## Call
+https://us-central1-mobilealerts-372415.cloudfunctions.net/apiproxy
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import escape
 import functions_framework
 
 @functions_framework.http
-def hello_http(request):
+def apiproxy(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
